a1732cde294de7d4a3f56d0b7f06695879592dd6
dulshanratnayake <dulshan.ratnayake@circles.asia>
2021-09-12 02:31:29 +0530

    added creation of docker mounting paths

--- deploy_ts3.sh
+++ deploy_ts3.sh
@@ -9,11 +9,15 @@ fi
 
 SERVERS="servers"
 PROJECT_FOLDER="ts3"
+DOCKER_MOUNT="docker_mount"
 
 PROJECT_PATH="~/${SERVERS}/${PROJECT_FOLDER}"
+DOCKER_MOUNT_PATH="~/${DOCKER_MOUNT}/${PROJECT_FOLDER}"
 # SERVER_PATH="~/${SERVERS}"
 
 ssh ${U_NAME}@${I_P} "mkdir -p ${PROJECT_PATH}"
+ssh ${U_NAME}@${I_P} "mkdir -p ${DOCKER_MOUNT_PATH}/teamspeak"
+ssh ${U_NAME}@${I_P} "mkdir -p ${DOCKER_MOUNT_PATH}/maria_db"
 
 scp -r scripts/ ${U_NAME}@${I_P}:${PROJECT_PATH}
 
